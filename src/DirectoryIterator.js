8c20e6d74e26ec7894f244cb5798a9284075d11d
Ely De La Cruz <ely.de.la.cruz@bloomingdales.com>
2017-05-04 10:36:37 -0400

    Backup on idea.

--- src/DirectoryIterator.js
+++ src/DirectoryIterator.js
@@ -6,6 +6,7 @@
 
 const fs = require('fs'),
     path = require('path'),
+    log = console.log.bind(console),
     {pureCurry3: curry3,
         pureCurry4: curry4} = require('fjl'),
 
@@ -34,7 +35,14 @@ const fs = require('fs'),
         .catch(reject))),
 
     processFile = curry3((filePath, zero, fileName) => new Promise ((resolve, reject) => readStat(filePath)
-        .then(processForkOnStat(filePath, zero, fileName))
+        .then(stat => {
+            const fileObj = {};
+            if (stat.isDirectory()) {
+                fileObj.files = [];
+                Object.assign(fileObj, stat);
+                processDirectory(filePath, fileObj)
+            }
+        })
         .then(resolve)
         .catch(reject))),
 
@@ -46,13 +54,116 @@ const fs = require('fs'),
             .catch(reject);
     })),
 
-    dirToJsonFromDir = (TypeRep, filterFn, dir) => readDirectory(dir).then(processFiles(dir, {})),
+    dirToJsonFromDir = (TypeRep, filterFn, dir) => readDirectory(dir).then(processFiles(dir, {})).then(log),
 
     dirToJsonFromFiles = (TypeRep, filterFn, files) => processFiles(Object, {}, files);
 
-
 function dirToJson (TypeRep, filterFn, dirOrFilesArray) {
-    return Array.isArray(dirOrFilesArray) ?
+    return (Array.isArray(dirOrFilesArray) ?
         dirToJsonFromFiles(TypeRep, filterFn, dirOrFilesArray) :
-        dirToJsonFromDir(TypeRep, filterFn, dirOrFilesArray);
+        dirToJsonFromDir(TypeRep, filterFn, dirOrFilesArray))
+        .then(log, log)
 }
+
+log(dirToJson(Object, () => {}, path.join(__dirname, '../node_modules')));
+
+class SjlFileInfo {
+
+    constructor (stat) {
+
+        let _fileName,
+            _filePath,
+            _files,
+            _lastChangedStatus,
+            _lastModified,
+            _createdDate,
+            _lastAccessed;
+
+        Object.defineProperties(this, {
+            fileName: {
+                get: function () {
+                    return _fileName;
+                },
+                set: function (value) {
+                    _fileName = value;
+                },
+                enumerable: true
+            },
+            filePath: {
+                get: function () {
+                    return _filePath;
+                },
+                set: function (value) {
+                    _filePath = value;
+                },
+                enumerable: true
+            },
+            basename: {
+                get: function () {
+                    return _basename;
+                },
+                set: function (value) {
+                    _basename = value;
+                },
+                enumerable: true
+            },
+            extension: {
+                get: function () {
+                    return _extension;
+                },
+                set: function (value) {
+                    _extension = value;
+                },
+                enumerable: true
+            },
+            lastModified: {
+                get: function () {
+                    return _lastModified;
+                },
+                set: function (value) {
+                    _lastModified = value;
+                },
+                enumerable: true
+            },
+            createdDate: {
+                get: function () {
+                    return _createdDate;
+                },
+                set: function (value) {
+                    _createdDate = value;
+                },
+                enumerable: true
+            },
+            lastChangedStatus: {
+                get: function () {
+                    return _lastChangedStatus;
+                },
+                set: function (value) {
+                    _lastChangedStatus = value;
+                },
+                enumerable: true
+            },
+            lastAccessed: {
+                get: function () {
+                    return _lastAccessed;
+                },
+                set: function (value) {
+                    _lastAccessed = value;
+                },
+                enumerable: true
+            },
+            files: {
+                get: function () {
+                    return _files;
+                },
+                set: function (value) {
+                    _files = value;
+                },
+                enumerable: true
+            },
+        });
+
+    }
+
+}
+
